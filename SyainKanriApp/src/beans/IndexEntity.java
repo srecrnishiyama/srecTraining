322afc897dfaef2521b6d5c608e417e1cc4e3289
user <user@192.168.0.9>
2019-01-13 12:49:57 +0900

    20190113勉強会 コメントの追加

--- SyainKanriApp/src/beans/IndexEntity.java
+++ SyainKanriApp/src/beans/IndexEntity.java
@@ -2,19 +2,21 @@ package beans;
 
 public class IndexEntity {
 	
-	private String loginId;
-	private String loginPassword;
-	private boolean kanriFlg;
+	private String loginId;			//ログインID
+	private String loginPassword; 	//ログインパスワード
+	private boolean kanriFlg;   	//管理フラグ(true=管理者権限あり　false=管理者権限なし)
 	
 	//コンストラクタ
 	public IndexEntity(){
 		
 	}
 	
+	//コンストラクタ（id）
 	public IndexEntity(String id){
 		this.loginId = id;
 	} 
 	
+	//コンストラクタ（id,password）
 	public IndexEntity(String id,String password){
 		this.loginId = id;
 		this.loginPassword = password;
